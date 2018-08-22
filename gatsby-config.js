743dfb0267cb154aa0b214b2fd52b5a4c020eebd
Josh Freeman <jdfreeman11@gmail.com>
2018-08-22 16:31:18 -0500

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Josh Freeman',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -14,7 +14,6 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        // icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
