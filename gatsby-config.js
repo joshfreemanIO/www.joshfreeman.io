76154f841dda93eaeff94e79a0af5d01f8370336
Josh Freeman <jdfreeman11@gmail.com>
2018-08-23 09:34:06 -0500

    Correct Favicon configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,17 +5,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-      },
-    },
     'gatsby-plugin-offline',
   ],
 }
--- src/components/Layout/Head.js
+++ src/components/Layout/Head.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import Favicons from './Favicons'
+import { withPrefix } from 'gatsby-link'
+
 
 const Head = ({data}) => (
   <Helmet>
@@ -8,7 +9,18 @@ const Head = ({data}) => (
     <meta name={''} content={''} />
     <html lang="en" />
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,400,600,700" rel="stylesheet" type="text/css" />
-    <Favicons />
+
+    {/* Favicons */}
+    <link rel="apple-touch-icon" sizes="180x180" href={ withPrefix("/favicon/apple-touch-icon.png")} />
+    <link rel="icon" type="image/png" sizes="32x32" href={ withPrefix("/favicon/favicon-32x32.png")} />
+    <link rel="icon" type="image/png" sizes="16x16" href={ withPrefix("/favicon/favicon-16x16.png")} />
+    <link rel="manifest" href={ withPrefix("/favicon/site.webmanifest")} />
+    <link rel="mask-icon" href={ withPrefix("/favicon/safari-pinned-tab.svg")} color="#ffffff" />
+    <link rel="shortcut icon" href={ withPrefix("/favicon/favicon.ico")} />
+    <meta name="msapplication-TileColor" content="#ffffff" />
+    <meta name="msapplication-config" content={withPrefix("/favicon/browserconfig.xml")} />
+    <meta name="theme-color" content="#ffffff" />
+
   </Helmet>
 )
 
