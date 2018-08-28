20f6476e9303b8c8327cf3638726b447f06b6e4a
Josh Freeman <jdfreeman11@gmail.com>
2018-08-28 14:44:49 -0500

    Configure blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,13 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/blog`,
+        name: "markdown-pages",
+      },
+    },
+    'gatsby-transformer-remark'
   ],
 }
