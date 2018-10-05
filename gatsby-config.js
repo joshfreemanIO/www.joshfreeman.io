9d650e7050d01fb24d2247f60c17ae3513482d4f
Josh Freeman <jdfreeman11@gmail.com>
2018-10-05 15:37:50 -0500

    Enable markdown blog posts
    https://www.gatsbyjs.org/docs/adding-markdown-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,13 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/posts/`,
+        name: "posts",
+      },
+    },
+    'gatsby-transformer-remark'
   ],
 }
