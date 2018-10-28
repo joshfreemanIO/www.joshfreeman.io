98e3aba03ba8280be63ba408fc6144fcad583f3e
Josh Freeman <jdfreeman11@gmail.com>
2018-10-28 14:20:19 -0500

    Relax

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import Layout from '../components/Layout'
 
 const Page = () => (
   <Layout>
-    <h1>Howdy, I'm Josh</h1>
+    <h1>Hi, I'm Josh</h1>
     <p>I'm based in San Antonio, Texas with the mission to create good in the world around me.</p>
-    <p>I'm programer, cellist, and all-around dabbler, so check out my blog to see what kinds of things may interest you.</p>
+    <p>I'm programer, builder, and all-around dabbler, so check out my blog to see what kinds of things may interest you.</p>
   </Layout>
 )
 
