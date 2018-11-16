5b6be96ad48a0350ea32ab1442ea967508d78e8c
Josh Freeman <jdfreeman11@gmail.com>
2018-11-16 16:38:11 -0600

    Correct grammar

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const Page = () => (
     <PageHeader>&nbsp;</PageHeader>
     <p>Hi, I'm Josh</p>
     <p>I'm based in San Antonio, Texas with the mission to create good in the world around me.</p>
-    <p>I'm programer, builder, and all-around dabbler, so check out my blog to see what kinds of things may interest you.</p>
+    <p>I'm a programer, builder, and all-around dabbler, so check out my blog to see what kinds of things may interest you.</p>
   </Layout>
 )
 
