f7f055ffdb7b635838dc8fde42d5375cec570c29
Josh Freeman <jdfreeman11@gmail.com>
2019-06-19 17:20:22 -0500

    Remove deprecated tests

--- src/projects/personal-finance/utils/__tests__/DollarsToCents.test.js
+++ src/projects/personal-finance/utils/__tests__/DollarsToCents.test.js
@@ -1,6 +1,5 @@
 import DollarsToCents from '../DollarsToCents'
 
 test('conversion of cents to dollars', () => {
-  console.log('$123.45'.replace(/^\$/, ''))
   expect(DollarsToCents('$123.45')).toBe(12345)
 })
