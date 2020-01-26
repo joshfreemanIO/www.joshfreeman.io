3260b0044aef3f773519d9dc185eb2c013304bf5
Josh Freeman <jdfreeman11@gmail.com>
2020-01-26 17:22:52 -0600

    Remove Saul from joshfreeman.io

--- src/pages/papers.js
+++ src/pages/papers.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 const Papers = () => (
   <>
     <h1>Papers</h1>
-    <ul>
-      <li><Link to="/papers/saul-of-tarsus/">Saul of Tarsus: The Leaven We Were Warned About</Link></li>
-    </ul>
   </>
 )
 
--- src/papers/saul-of-tarsus/data.md
+++
@@ -1,8 +0,0 @@
----
-paperdata: true
-paper: "saul-of-tarsus"
-title: "Saul of Tarsus: The Leaven We Were Warned About"
-template: "src/templates/paper.js"
-cover: "src/papers/saul-of-tarsus/cover.js"
----
-
