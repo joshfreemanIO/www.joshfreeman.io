29b8aabf2e8e3e98ac41ff7736f869f6e45edd49
Josh Freeman <jdfreeman11@gmail.com>
2020-01-26 18:34:27 -0600

    Update summary

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const Page = () => (
     <PageHeader>&nbsp;</PageHeader>
     <p>Hi, I&apos;m Josh</p>
     <p>I&apos;m based in San Antonio, Texas with the mission to create good in the world around me.</p>
-    <p>I&apos;m a programer, builder, and all-around dabbler, so check out my blog to see what kinds of things may interest you.</p>
+    <p>I authored Perspective and lead tech at Sendpark.</p>
   </>
 )
 
